b513b693d9050233ce17c79e803faeed967fa604
G558253 <G558253@bnr-lap558253.ADS.LOCAL>
2020-03-15 10:48:32 +0100

    change A

--- onlinebookstore/src/main/java/com/hamzastore/onlinebookstore/OnlineBookStoreApplication.java
+++ onlinebookstore/src/main/java/com/hamzastore/onlinebookstore/OnlineBookStoreApplication.java
@@ -3,6 +3,11 @@ package com.hamzastore.onlinebookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*change A
+ * 
+ * 
+ */
+
 @SpringBootApplication
 public class OnlineBookStoreApplication {
 
